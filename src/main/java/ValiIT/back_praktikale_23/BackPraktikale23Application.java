552e6aa6bd35c4d92107626de65f283f74167167
Malle <mallevooremaa@gmail.com>
2023-05-16 15:42:56 +0300

    Inital commit

---
+++ src/main/java/ValiIT/back_praktikale_23/BackPraktikale23Application.java
@@ -0,0 +1,13 @@
+package ValiIT.back_praktikale_23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPraktikale23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackPraktikale23Application.class, args);
+	}
+
+}
---
+++ src/test/java/ValiIT/back_praktikale_23/BackPraktikale23ApplicationTests.java
@@ -0,0 +1,13 @@
+package ValiIT.back_praktikale_23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPraktikale23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
