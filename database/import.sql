19f439e670e1cec643371231cf14131182da729a
Malle <mallevooremaa@gmail.com>
2023-05-16 16:51:41 +0300

    database import poolel teel

---
+++ database/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Pärnu');
+INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Tallinn');
+INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Tartu');
+
+INSERT INTO public.category (id, name) VALUES (DEFAULT, 'Kokandus');
+INSERT INTO public.category (id, name) VALUES (DEFAULT, 'Ehitus');
+INSERT INTO public.category (id, name) VALUES (DEFAULT, 'Infotehnoloogia');
