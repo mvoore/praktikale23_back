05e4b9fa8abdb27b25fdeb8290f0931454785af2
Liisi M <liisi.muur@gmail.com>
2023-05-18 09:26:26 +0300

    LoginService WIP Rain tegi korda

--- src/main/java/ValiIT/back_praktikale_23/business/login/LoginResponse.java
+++ src/main/java/ValiIT/back_praktikale_23/business/login/LoginResponse.java
@@ -1,14 +1,10 @@
 package ValiIT.back_praktikale_23.business.login;
 
-import ValiIT.back_praktikale_23.domain.user.User;
-import lombok.Value;
+import lombok.Data;
 
 import java.io.Serializable;
 
-/**
- * DTO for {@link User}
- */
-@Value
+@Data
 public class LoginResponse implements Serializable {
     private Integer userId;
     private String roleName;
