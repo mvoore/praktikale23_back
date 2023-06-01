4f18335c5578eeb64443240e57de234c7676dc11
Liisi M <liisi.muur@gmail.com>
2023-06-01 13:21:01 +0300

    company mapper fix ja companydto fix

--- src/main/java/ValiIT/back_praktikale_23/business/internship/company/dto/CompanyDto.java
+++ src/main/java/ValiIT/back_praktikale_23/business/internship/company/dto/CompanyDto.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CompanyDto implements Serializable {
+    private Integer companyId;
     private String companyName;
     private String phoneNumber;
     private String email;
